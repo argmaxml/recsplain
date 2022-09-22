3728b6c0fce1ad89f69dbd92f0f1e6ad9e111532
KempinskiB <benjaminkempinski@gmail.com>
2022-09-22 14:29:01 +0300

    fixing requirements

--- recsplain/__init__.py
+++ recsplain/__init__.py
@@ -1,4 +1,4 @@
-__version__="0.0.83"
+__version__="0.0.84"
 from .strategies import BaseStrategy, AvgUserStrategy, RedisStrategy
 from .encoders import PartitionSchema
 from .endpoint import run_server
